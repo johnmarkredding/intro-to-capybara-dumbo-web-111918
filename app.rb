3ff2031897b97a570ff3147d879f8e0c927d5407
John Mark Redding <johnmarkredding@gmail.com>
2018-12-11 14:38:50 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 class Application < Sinatra::Base
-  # Write your code here!
-
+  
+  get '/' do
+    erb :index
+  end
+  
+  get '/greeting' do
+    erb :greet
+  end
+  
+  post '/greet' do
+    "Hello World"
+  end
 end
